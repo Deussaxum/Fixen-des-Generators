e24670ba28c627179ccdcab36e95f5ebdd228446
Deussaxum <147807491+Deussaxum@users.noreply.github.com>
2023-12-14 11:22:00 +0100

    ss

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-mport streamlit as st
+import streamlit as st
 import subprocess
 
 def build_latex_code(name, address, phone, email, qualification1, qualification2, qualification3, university1, locationus1, majorus1, timeus1, courses1, gpa1, clubs1, university2, locationus2, majorus2, timeus2, courses2, gpa2, clubs2, experience1, locatione1, position1, timee1, task11, task12, task13, experience2, locatione2, position2, timee2, task21, task22, task23, experience3, locatione3, position3, timee3, task31, task32, task33, extracurricular1, additionaleducation1, certificates1):
